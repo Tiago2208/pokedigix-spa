b4b15d003231b5a4fd6d0ea0542b81c0f4dfb139
Tiago Andrade <tiago22900@gmail.com>
2022-09-20 18:05:28 -0400

    iniciando um projeto

---
+++ src/services/PokemonDataService.js
@@ -0,0 +1,43 @@
+import http from "../http-commons";
+
+class PokemonDataService {
+   async buscarTodos() {
+        let resposta = await http.get('/pokemons');
+        return resposta.data;
+    }
+
+    async buscarPeloId(id) {
+        let resposta = await http.get('/pokemons/${id}');
+        return resposta.data;
+    }
+
+    async criar(pokemon) {
+        let resposta = await http.post('/pokemons', pokemon);
+        return resposta.data;
+    }
+
+    async atualizar(id, pokemon) {
+        let resposta = await http.put('/pokemons/${id}', pokemon);
+        return resposta.data;
+    }
+    
+    async remover(id) {
+        let resposta = await http.delete('/pokemons/${id}');
+    }
+
+    async buscarPeloNome(nome) {
+        let resposta = await http.get('/pokemons?termo=${nome}');
+        return resposta.data;
+    }
+
+    async remover(nome) {
+        let resposta = await http.delete('/pokemons?termo=${nome}');
+    }
+
+    async buscarPeloIdDoTipo(id) {
+        let resposta = await http.get('/pokemons/tipo/${id}');
+        return resposta.data;
+    }
+}
+
+export default new PokemonDataService();
